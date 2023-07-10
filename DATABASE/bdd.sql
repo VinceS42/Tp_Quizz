e190d1c7551b6bcbe0058e13beec0a1141164ad9
AbderrezakMehenni <mehenni5abderrezak@gmail.com>
2023-07-10 13:37:19 +0200

    bdd modified

--- DATABASE/bdd.sql
+++ DATABASE/bdd.sql
@@ -3,24 +3,30 @@ CREATE DATABASE IF NOT EXISTS quizz;
 USE quizz;
 
 CREATE TABLE users(
-   id_user INT AUTO_INCREMENT NOT NULL,
+   id_user INT,
    pseudo VARCHAR(150) NOT NULL,
    PRIMARY KEY(id_user),
    UNIQUE(pseudo)
 );
 
 CREATE TABLE questions(
-   id_question INT AUTO_INCREMENT NOT NULL,
+   id_question INT,
    question TEXT NOT NULL,
    reponse TEXT NOT NULL,
    PRIMARY KEY(id_question)
 );
 
 CREATE TABLE scores(
-   id_score INT AUTO_INCREMENT NOT NULL,
-   points BIGINT NOT NULL,
+   id_score INT,
+   point BIGINT NOT NULL,
    dateTime DATETIME NOT NULL,
    PRIMARY KEY(id_score)
+);
+
+CREATE TABLE fake_reponse(
+   id_fake_reponse INT,
+   fake_reponse TEXT NOT NULL,
+   PRIMARY KEY(id_fake_reponse)
 );
 
 CREATE TABLE id_user_question(
@@ -37,4 +43,12 @@ CREATE TABLE id_user_score(
    PRIMARY KEY(id_user, id_score),
    FOREIGN KEY(id_user) REFERENCES users(id_user),
    FOREIGN KEY(id_score) REFERENCES scores(id_score)
+);
+
+CREATE TABLE id_question_reponse(
+   id_question INT,
+   id_fake_reponse INT,
+   PRIMARY KEY(id_question, id_fake_reponse),
+   FOREIGN KEY(id_question) REFERENCES questions(id_question),
+   FOREIGN KEY(id_fake_reponse) REFERENCES fake_reponse(id_fake_reponse)
 );
