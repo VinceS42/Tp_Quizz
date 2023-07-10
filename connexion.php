f981b2545c169cec28deea9cd51a95d86b2b8417
AbderrezakMehenni <mehenni5abderrezak@gmail.com>
2023-07-10 15:31:53 +0200

    connexion finish

--- connexion.php
+++ connexion.php
@@ -14,25 +14,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // var_dump($result);
 
     if (count($result) > 0) {
+        $_SESSION['pseudo'] = $pseudo;
         header("Location: index.php");
     } else {
         $query = "INSERT INTO users (pseudo) VALUES (:pseudo)";
         $stmt = $db->prepare($query);
         $stmt->bindParam(':pseudo', $pseudo);
         $stmt->execute();
-
+        $_SESSION['pseudo'] = $pseudo;
         header("Location: index.php");
         exit();
     }
 }
 ?>
-<section>
-    <form method="POST">
-        <input type="text" name="pseudo" placeholder="Votre pseudo">
-        <button type="submit" name="connexion">Se connecter</button>
-    </form>
+<section class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="bg-test col-6 mt-5 mb-5 p-5 rounded-4">
+            <form method="POST" class="text-center">
+                <input type="text" name="pseudo" class=" input form-control mb-5" size="80" placeholder="Votre pseudo">
+                <button type="submit" name="connexion" class="btn btn-primary">Se connecter</button>
+            </form>
+        </div>
+    </div>
 </section>
 
+
 <?php
-    include_once('./partials/footer.php');
+include_once('./partials/footer.php');
 ?>
