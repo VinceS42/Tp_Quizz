54ed7dfeb910639493a93d4fb39ad51c72b8f4a8
VinceS42 <v.fayon.pro@gmail.com>
2023-07-13 15:38:40 +0200

    Fini

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('utils/db-connect.php'); // J'appelle la base de données
 
 include_once('partials/header.php');
 
-include_once('./process/question.php');
+include_once('question.php');
 
 include_once('partials/footer.php');
 ?>
